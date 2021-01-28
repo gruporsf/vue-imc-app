61a5866649c8c45606446655490b23d25368c23f
Ricardo <gruporsf@gmail.com>
2021-01-28 14:47:23 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Calculo do IMC</h1>
+
+  <h2>Digite seu peso e sua altura para calcular o IMC</h2>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" :disabled="imc" />
+      <label for="input-weight">Peso</label>
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="height" :disabled="imc" />
+      <label for="input-height">Altura</label>
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <p>
+      <Button label="Calcular" @click="calculate()" class="p-button-success" style="margin-right:10px;" />
+      <Button label="Limpar" @click="clear()" />
+    </p>
+  </div>
+
+  <div v-if="imc">
+    <p class="label-result">Seu IMC é: {{ imc }}</p>
+    <p class="label-classification">A classificação do seu IMC é: {{ classification }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      height: null,
+      weight: null,
+      imc: null,
+      classification: null
+    }
+  },
+  methods: {
+    calculate: function() {
+      this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+
+      if(this.imc < 18.5){
+        this.classification = "Magreza";
+      }
+      else if(this.imc >= 18.5 && this.imc < 25){
+        this.classification = "Normal";
+      }
+      else if(this.imc >= 25 && this.imc < 30){
+        this.classification = "Sobrepeso";
+      }
+      else if(this.imc >= 30 && this.imc < 40){
+        this.classification = "Obesidade";
+      }
+      else{
+        this.classification = "Obsidade Grave";
+      }
+    },
+    clear: function(){
+      this.height = null;
+      this.weight = null;
+      this.imc = null;
+      this.classification = null;
+    }
   }
 }
 </script>
@@ -19,8 +76,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.div-imc{
+  margin-top: 2rem;
+}
+
+.label-result{
+  font-size: 1.6rem;
+}
+
+.label-classification{
+  font-size: 1.2rem;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+import 'primevue/resources/themes/saga-blue/theme.css';       //theme
+import 'primevue/resources/primevue.min.css';                 //core css
+//import 'primeicons/primeicons.css';                           //icons
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component("Button", Button);
+app.component("InputText", InputText);
+
+app.mount('#app');
